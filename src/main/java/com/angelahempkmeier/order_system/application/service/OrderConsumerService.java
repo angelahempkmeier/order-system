25d5fd213f0bdfdbb4b2274714583bf14e0c71b1
Angela Hempkmeier <aggsdpz@gmail.com>
2024-07-01 22:03:33 -0300

    feat: creation of order consumer and producer, adding logs to simplify the flow reading

---
+++ src/main/java/com/angelahempkmeier/order_system/application/service/OrderConsumerService.java
@@ -0,0 +1,26 @@
+package com.angelahempkmeier.order_system.application.service;
+
+import com.angelahempkmeier.order_system.domain.model.Order;
+import com.angelahempkmeier.order_system.domain.model.OrderStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+@Service
+public class OrderConsumerService {
+
+    private final OrderService orderService;
+    private static final Logger logger = LoggerFactory.getLogger(OrderConsumerService.class);
+
+
+    public OrderConsumerService(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @KafkaListener(topics = "orders", groupId = "order-group")
+    public void consume(Order order) {
+        logger.info("Received message from Kafka: {}", order);
+        orderService.updateOrderStatus(order.getId(), OrderStatus.ENVIADO_TRANSPORTADORA);
+        logger.info("Order status updated to ENVIADO_TRANSPORTADORA for Order ID: {}", order.getId());
+    }
+}
---
+++ src/main/java/com/angelahempkmeier/order_system/application/service/OrderProducerService.java
@@ -0,0 +1,25 @@
+package com.angelahempkmeier.order_system.application.service;
+
+import com.angelahempkmeier.order_system.domain.model.Order;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderProducerService {
+
+    private final KafkaTemplate<String, Order> kafkaTemplate;
+    private static final Logger logger = LoggerFactory.getLogger(OrderProducerService.class);
+
+
+    public OrderProducerService(KafkaTemplate<String, Order> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendOrder(Order order) {
+        logger.info("Sending order to Kafka: {}", order);
+        kafkaTemplate.send("orders", order);
+        logger.info("Order sent to Kafka: {}", order);
+    }
+}
