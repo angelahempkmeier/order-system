1acb28ad9d7b203047666060c4814c005d8a9a94
Angela Hempkmeier <aggsdpz@gmail.com>
2024-07-04 16:24:29 -0300

    feat: creation of an generation id method with random string based on current date and creation of and personalized constructor

--- src/main/java/com/angelahempkmeier/order_system/domain/model/Order.java
+++ src/main/java/com/angelahempkmeier/order_system/domain/model/Order.java
@@ -3,6 +3,10 @@ package com.angelahempkmeier.order_system.domain.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
+
 @Document(collection = "orders")
 public class Order {
     @Id
@@ -11,7 +15,14 @@ public class Order {
     private int quantity;
     private OrderStatus status;
 
-    public Order() {
+    public Order(){
+
+    }
+
+    public Order(String product, int quantity){
+        this.id = generateRandomString();
+        this.product = product;
+        this.quantity = quantity;
     }
 
     public Order(String id, String product, int quantity, OrderStatus status) {
@@ -61,5 +72,16 @@ public class Order {
                 ", quantity=" + quantity +
                 ", status=" + status +
                 '}';
+    }
+
+    private String generateRandomString() {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHH") ;
+        String formattedDateTime = now.format(formatter);
+
+        Random random = new Random();
+        int randomNumber = random.nextInt(1000);
+
+        return formattedDateTime + String.format("%03d", randomNumber);
     }
 }
